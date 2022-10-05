ad97910efd3e5d725793fbfc4728a4ba0cb6aae2
Taro Makino <taro@nyu.edu>
2022-10-04 20:01:45 -0400

    Make modelnet40 training consistent with toy problem

--- causal_multimodal_learning/modelnet40/model.py
+++ causal_multimodal_learning/modelnet40/model.py
@@ -1,65 +1,103 @@
-import torch
-import torch.nn.functional as F
 import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import torchvision
-from utils.stats import gaussian_kld, prior_kld
-from torch import nn
-from torch.optim import Adam
+from utils.nn_utils import MLP, device
+from utils.stats import make_gaussian, prior_kld
+from torch.optim import AdamW
 
-def make_resnet_encoder(): # Replace with pl version
-    model = torchvision.models.resnet50(pretrained=True)
-    model.fc = nn.Identity(model.fc.out_features)
-    return model
+N_CLASSES = 40
 
-class InputEncoder(nn.Module):
-    def __init__(self, latent_dim):
+def make_resnet_embedder():
+    model = torchvision.models.resnet18(pretrained=True)
+    embed_dim = model.fc.in_features
+    model.fc = nn.Identity(embed_dim)
+    return model, embed_dim
+
+class GaussianNetwork(nn.Module):
+    def __init__(self, hidden_dims, output_dim, is_y_input):
         super().__init__()
-        self.encoder_x0 = make_resnet_encoder()
-        self.encoder_x1 = make_resnet_encoder()
-        self.fc_mu = nn.Linear(2 * 2048, latent_dim)
-        self.fc_var = nn.Linear(2 * 2048, latent_dim)
+        self.is_y_input = is_y_input
+        self.img_embedder0, embed_dim0 = make_resnet_embedder()
+        self.img_embedder1, embed_dim1 = make_resnet_embedder()
+        in_dim = embed_dim0 + embed_dim1 + (40 if is_y_input else 0)
+        self.mu_net = MLP(in_dim, hidden_dims, output_dim)
+        self.logvar_net = MLP(in_dim, hidden_dims, output_dim)
 
-    def forward(self, x0, x1):
-        x0 = self.encoder_x0(x0)
-        x1 = self.encoder_x1(x1)
-        merged = torch.cat((x0, x1), dim=1)
-        return self.fc_mu(merged), self.fc_var(merged)
-
-class InputTargetEncoder(nn.Module):
-    def __init__(self, latent_dim):
-        super().__init__()
-        self.encoder_x0 = make_resnet_encoder()
-        self.encoder_x1 = make_resnet_encoder()
-        self.fc_mu = nn.Linear(2 * 2048 + 1, latent_dim)
-        self.fc_var = nn.Linear(2 * 2048 + 1, latent_dim)
-
-    def forward(self, x0, x1, y):
-        x0 = self.encoder_x0(x0)
-        x1 = self.encoder_x1(x1)
-        merged = torch.cat((x0, x1, y[:, None]), dim=1)
-        return self.fc_mu(merged), self.fc_var(merged)
+    def forward(self, *args):
+        if self.is_y_input:
+            x0, x1, y = args
+            x0_embed = self.img_embedder0(x0)
+            x1_embed = self.img_embedder1(x1)
+            mu = self.mu_net(x0_embed, x1_embed, y)
+            logvar = self.logvar_net(x0_embed, x1_embed, y)
+        else:
+            x0, x1 = args
+            x0_embed = self.img_embedder0(x0)
+            x1_embed = self.img_embedder1(x1)
+            mu = self.mu_net(x0_embed, x1_embed)
+            logvar = self.logvar_net(x0_embed, x1_embed)
+        return mu, logvar
 
 class Decoder(nn.Module):
-    def __init__(self, latent_dim):
+    def __init__(self, hidden_dims, latent_dim):
         super().__init__()
-        self.encoder_x0 = make_resnet_encoder()
-        self.encoder_x1 = make_resnet_encoder()
-        self.fc_y = nn.Linear(2 * 2048 + latent_dim, 40)
+        self.img_embedder0, embed_dim0 = make_resnet_embedder()
+        self.img_embedder1, embed_dim1 = make_resnet_embedder()
+        in_dim = embed_dim0 + embed_dim1 + latent_dim
+        self.y_net = MLP(in_dim, hidden_dims, N_CLASSES)
 
     def forward(self, x0, x1, z):
-        x0 = self.encoder_x0(x0)
-        x1 = self.encoder_x1(x1)
-        return self.fc_y(torch.cat((x0, x1, z), dim=1))
+        x0_embed = self.img_embedder0(x0)
+        x1_embed = self.img_embedder1(x1)
+        return self.y_net(x0_embed, x1_embed, z)
 
-class SemiSupervisedVae(pl.LightningModule):
-    def __init__(self, lr, latent_dim, alpha):
+class PosteriorX(pl.LightningModule):
+    def __init__(self, hidden_dims, latent_dim, lr, wd, batch_size):
         super().__init__()
         self.save_hyperparameters()
         self.lr = lr
-        self.alpha = alpha
-        self.input_target_encoder = InputTargetEncoder(latent_dim)
-        self.input_encoder = InputEncoder(latent_dim)
-        self.decoder = Decoder(latent_dim)
+        self.wd = wd
+        self.encoder_xy = GaussianNetwork(hidden_dims, latent_dim, N_CLASSES)
+        self.encoder_x = GaussianNetwork(hidden_dims, latent_dim, 0)
+        self.prior = make_gaussian(torch.zeros((batch_size, latent_dim), device=device()), torch.zeros((batch_size,
+            latent_dim), device=device()))
+
+    def loss(self, x0, x1, y):
+        mu_xy, logvar_xy = self.encoder_xy(x0, x1, F.one_hot(y, N_CLASSES))
+        mu_x, logvar_x = self.encoder_x(x0, x1)
+        posterior_xy_dist = make_gaussian(mu_xy.clone().detach(), logvar_xy.clone().detach())
+        posterior_x_dist = make_gaussian(mu_x, logvar_x)
+        loss = torch.distributions.kl_divergence(posterior_xy_dist, posterior_x_dist).mean()
+        kld = prior_kld(mu_x, logvar_x).mean()
+        return loss, kld
+
+    def training_step(self, batch, batch_idx):
+        loss, kld = self.loss(*batch)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss, kld = self.loss(*batch)
+        self.log("val_loss", loss, on_step=False, on_epoch=True)
+        self.log("val_kld", kld, on_step=False, on_epoch=True)
+
+    def test_step(self, batch, batch_idx):
+        loss, kld = self.loss(*batch)
+        self.log("test_loss", loss, on_step=False, on_epoch=True)
+        self.log("test_kld", kld, on_step=False, on_epoch=True)
+
+    def configure_optimizers(self):
+        return AdamW(self.encoder_x.parameters(), lr=self.lr, weight_decay=self.wd)
+
+class SemiSupervisedVae(pl.LightningModule):
+    def __init__(self, hidden_dims, latent_dim, lr, wd):
+        super().__init__()
+        self.save_hyperparameters()
+        self.lr = lr
+        self.wd = wd
+        self.encoder = GaussianNetwork(hidden_dims, latent_dim, N_CLASSES)
+        self.decoder = Decoder(hidden_dims, latent_dim)
 
     def sample_z(self, mu, logvar):
         if self.training:
@@ -70,32 +108,26 @@ class SemiSupervisedVae(pl.LightningModule):
             return mu
 
     def loss(self, x0, x1, y):
-        # Vanilla VAE loss
-        mu_input_target, logvar_input_target = self.input_target_encoder(x0, x1, y)
-        z = self.sample_z(mu_input_target, logvar_input_target)
+        mu_z, logvar_z = self.encoder(x0, x1, F.one_hot(y, N_CLASSES))
+        kld_loss = prior_kld(mu_z, logvar_z)
+        z = self.sample_z(mu_z, logvar_z)
         y_reconst = self.decoder(x0, x1, z)
         reconst_loss = F.cross_entropy(y_reconst, y, reduction="none")
-        posterior_kld_loss = prior_kld(mu_input_target, logvar_input_target)
-        # KL(q(z | x, x', y) || q(z | x, x')), don't backprop through q(z | x, x', y)
-        mu_input, logvar_input = self.input_encoder(x0, x1)
-        gaussian_kld_loss = gaussian_kld(mu_input_target.clone().detach(), logvar_input_target.clone().detach(),
-            mu_input, logvar_input)
-        return reconst_loss, posterior_kld_loss, gaussian_kld_loss
+        return kld_loss, reconst_loss
 
     def training_step(self, batch, batch_idx):
-        reconst_loss, posterior_kld_loss, gaussian_kld_loss = self.loss(*batch)
-        return (reconst_loss + posterior_kld_loss + self.alpha * gaussian_kld_loss).mean()
+        kld_loss, reconst_loss = self.loss(*batch)
+        return (kld_loss + reconst_loss).mean()
 
     def validation_step(self, batch, batch_idx):
-        reconst_loss, posterior_kld_loss, gaussian_kld_loss = self.loss(*batch)
-        self.log("val_posterior_kld_loss", posterior_kld_loss.mean(), on_step=False, on_epoch=True)
-        self.log("val_gaussian_kld_loss", gaussian_kld_loss.mean(), on_step=False, on_epoch=True)
-        self.log("val_loss", (reconst_loss + posterior_kld_loss).mean(), on_step=False, on_epoch=True)
+        kld_loss, reconst_loss = self.loss(*batch)
+        self.log("val_loss", (kld_loss + reconst_loss).mean(), on_step=False, on_epoch=True)
+        self.log("val_kld_loss", kld_loss.mean(), on_step=False, on_epoch=True)
 
     def test_step(self, batch, batch_idx):
-        reconst_loss, posterior_kld_loss, gaussian_kld_loss = self.loss(*batch)
-        self.log("test_posterior_kld_loss", posterior_kld_loss.mean(), on_step=False, on_epoch=True)
-        self.log("test_loss", (reconst_loss + posterior_kld_loss).mean(), on_step=False, on_epoch=True)
+        kld_loss, reconst_loss = self.loss(*batch)
+        self.log("test_loss", (kld_loss + reconst_loss).mean(), on_step=False, on_epoch=True)
+        self.log("test_kld_loss", kld_loss.mean(), on_step=False, on_epoch=True)
 
     def configure_optimizers(self):
-        return Adam(self.parameters(), self.lr)
+        return AdamW(self.parameters(), lr=self.lr, weight_decay=self.wd)
--- causal_multimodal_learning/modelnet40/train.py
+++ causal_multimodal_learning/modelnet40/train.py
@@ -2,28 +2,36 @@ import os
 import pytorch_lightning as pl
 from argparse import ArgumentParser
 from utils.file import save_file
-from utils.stats import make_trainer
+from utils.nn_utils import make_trainer
 from modelnet40.data import make_data
-from modelnet40.model import SemiSupervisedVae
+from modelnet40.model import SemiSupervisedVae, PosteriorX
 
 def main(args):
-    save_file(args, os.path.join(args.dpath, f"version_{args.seed}", "args.pkl"))
-    pl.seed_everything(args.seed)
-    model = SemiSupervisedVae(args.lr, args.latent_dim, args.alpha)
+    seed = args.__dict__.pop("seed")
+    save_file(args, os.path.join(args.dpath, "args.pkl"))
+    pl.seed_everything(seed)
     data_train, data_val, data_test = make_data(args.batch_size, args.n_workers)
-    trainer = make_trainer(args.dpath, args.seed, args.n_epochs, args.patience)
-    trainer.fit(model, data_train, data_val)
-    trainer.test(model, data_test)
+    vae = SemiSupervisedVae(args.hidden_dims, args.latent_dim, args.lr_vae, args.wd)
+    vae_trainer = make_trainer(os.path.join(args.dpath, "vae"), seed, args.n_epochs, args.patience)
+    vae_trainer.fit(vae, data_train, data_val)
+    vae_trainer.test(vae, data_test)
+    posterior_x = PosteriorX(args.hidden_dims, args.latent_dim, args.lr_posterior_x, args.wd, args.batch_size)
+    posterior_x.encoder_xy.load_state_dict(vae.encoder.state_dict())
+    posterior_x_trainer = make_trainer(os.path.join(args.dpath, "posterior_x"), seed, args.n_epochs, args.patience)
+    posterior_x_trainer.fit(posterior_x, data_train, data_val)
+    posterior_x_trainer.test(posterior_x, data_test)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--dpath", type=str, default="results")
     parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--lr_vae", type=float, default=1e-3)
+    parser.add_argument("--lr_posterior_x", type=float, default=1e-3)
+    parser.add_argument("--wd", type=float, default=1e-5)
     parser.add_argument("--n_epochs", type=int, default=200)
     parser.add_argument("--patience", type=int, default=20)
-    parser.add_argument("--batch_size", type=int, default=50)
+    parser.add_argument("--batch_size", type=int, default=100)
     parser.add_argument("--n_workers", type=int, default=20)
-    parser.add_argument("--latent_dim", type=int, default=100)
-    parser.add_argument("--alpha", type=float, default=0)
+    parser.add_argument("--hidden_dims", nargs="+", type=int, default=[512, 512])
+    parser.add_argument("--latent_dim", type=int, default=256)
     main(parser.parse_args())
