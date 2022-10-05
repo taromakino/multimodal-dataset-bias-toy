484b6b26dd6c11769ab5d43afd25ce0a4c838b87
Taro Makino <taro@nyu.edu>
2022-10-05 14:24:40 -0400

    Minor changes

--- causal_multimodal_learning/modelnet40/inference.py
+++ causal_multimodal_learning/modelnet40/inference.py
@@ -31,7 +31,8 @@ def main(args):
                 mu_x, logvar_x = posterior_x.encoder_x(x0, x1)
                 posterior_x_dist = make_gaussian(mu_x, logvar_x)
                 z = posterior_x_dist.sample((args.n_samples,))
-                x0_rep, x1_rep = x0.repeat(args.n_samples, 1), x1.repeat(args.n_samples, 1)
+                x0_rep = torch.repeat_interleave(x0, repeats=args.n_samples, dim=0)
+                x1_rep = torch.repeat_interleave(x1, repeats=args.n_samples, dim=0)
 
                 y_mu, y_logvar = vae.decoder(x0_rep, x1_rep, z)
                 decoder_dist = make_gaussian(y_mu, y_logvar)
