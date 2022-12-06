265e397097f028a12ff4c8616a08c455db181be2
Taro Makino <taro@nyu.edu>
2022-12-06 14:47:13 -0500

    Make y noise large

--- causal_multimodal_learning/toy_problem/data.py
+++ causal_multimodal_learning/toy_problem/data.py
@@ -21,18 +21,18 @@ def make_dataloader(data_tuple, batch_size, n_workers, is_train):
     return DataLoader(TensorDataset(*data_tuple), shuffle=is_train, batch_size=batch_size, num_workers=n_workers,
         pin_memory=True, persistent_workers=True)
 
-def make_dataset(seed, n_examples, data_dim, is_spurious):
+def make_raw_data(seed, n_examples, data_dim, is_spurious):
     rng = np.random.RandomState(seed)
     if data_dim == 1:
         u = rng.normal(size=n_examples).astype("float32")
         x0_noise = rng.normal(size=n_examples).astype("float32")
         x1_noise = rng.normal(size=n_examples).astype("float32")
-        y_noise = rng.normal(loc=0, scale=0.1, size=n_examples).astype("float32")
+        y_noise = rng.normal(loc=0, scale=10, size=n_examples).astype("float32")
     else:
         u = rng.multivariate_normal(mean=np.zeros(data_dim), cov=np.eye(data_dim), size=n_examples).astype("float32")
         x0_noise = rng.multivariate_normal(mean=np.zeros(data_dim), cov=np.eye(data_dim), size=n_examples).astype("float32")
         x1_noise = rng.multivariate_normal(mean=np.zeros(data_dim), cov=np.eye(data_dim), size=n_examples).astype("float32")
-        y_noise = rng.multivariate_normal(mean=np.zeros(data_dim), cov=np.diag(np.repeat(0.1**2, data_dim)),
+        y_noise = rng.multivariate_normal(mean=np.zeros(data_dim), cov=np.diag(np.repeat(10 ** 2, data_dim)),
             size=n_examples).astype("float32")
     x0 = u + x0_noise
     x1 = u**2 + x1_noise
@@ -49,13 +49,13 @@ def make_dataset(seed, n_examples, data_dim, is_spurious):
 
 def make_data(seed, n_examples, train_ratio, data_dim, batch_size, n_workers):
     n_trainval, n_test = n_examples
-    x0_trainval, x1_trainval, y_trainval, u_trainval = make_dataset(seed, n_trainval, data_dim, True)
+    x0_trainval, x1_trainval, y_trainval, u_trainval = make_raw_data(seed, n_trainval, data_dim, True)
     n_train = int(len(x0_trainval) * train_ratio)
     x0_train, x1_train, y_train, u_train = x0_trainval[:n_train], x1_trainval[:n_train], y_trainval[:n_train], \
         u_trainval[:n_train]
     x0_val, x1_val, y_val, u_val = x0_trainval[n_train:], x1_trainval[n_train:], y_trainval[n_train:], \
         u_trainval[n_train:]
-    x0_test, x1_test, y_test, u_test = make_dataset(2**32 - 1, n_test, data_dim, False)
+    x0_test, x1_test, y_test, u_test = make_raw_data(2 ** 32 - 1, n_test, data_dim, False)
 
     x0_train, x0_val, x0_test = normalize(x0_train, x0_val, x0_test)
     x1_train, x1_val, x1_test = normalize(x1_train, x1_val, x1_test)
