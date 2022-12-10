f9babd26bc00bad38e2bb3f3210a18f2422b5597
Taro Makino <taro@nyu.edu>
2022-12-10 16:20:12 -0500

    Dropout

--- causal_multimodal_learning/utils/nn_utils.py
+++ causal_multimodal_learning/utils/nn_utils.py
@@ -14,6 +14,7 @@ class MLP(nn.Module):
         for hidden_dim in hidden_dims:
             module_list.append(nn.Linear(last_in_dim, hidden_dim))
             module_list.append(nn.SiLU())
+            module_list.append(nn.Dropout())
             last_in_dim = hidden_dim
         self.module_list = nn.Sequential(*module_list)
         if isinstance(output_dims, list):
