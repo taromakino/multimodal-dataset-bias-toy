459a859e1e4fcc88fd9289a60d9f3865d2a3732c
Taro Makino <taro@nyu.edu>
2023-02-07 13:58:52 -0500

    Remove is_gpu

--- multimodal_dataset_bias_toy/utils/nn_utils.py
+++ multimodal_dataset_bias_toy/utils/nn_utils.py
@@ -1,12 +1,13 @@
 import pytorch_lightning as pl
+import torch
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import CSVLogger
 
 
-def make_trainer(dpath, seed, n_steps, is_gpu):
+def make_trainer(dpath, seed, n_steps):
     return pl.Trainer(
         logger=CSVLogger(dpath, name="", version=seed),
         callbacks=[
             ModelCheckpoint(monitor="val_loss", filename="best")],
         max_steps=n_steps,
-        accelerator="gpu" if is_gpu else "cpu")
+        accelerator="gpu" if torch.cuda.is_available() else "cpu")
