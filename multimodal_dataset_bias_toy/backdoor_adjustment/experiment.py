7423edd3a7ed03d0f86bd0491c9661fcc6e0fdef
Taro Makino <taro@nyu.edu>
2023-02-07 13:35:44 -0500

    n_steps instead of n_epochs

--- multimodal_dataset_bias_toy/backdoor_adjustment/experiment.py
+++ multimodal_dataset_bias_toy/backdoor_adjustment/experiment.py
@@ -16,7 +16,7 @@ def main(config):
         config.train_ratio, config.u_sd, config.x_sd, config.y_sd, config.s_shift, config.batch_size, config.n_workers)
     model = Model(seed, config.dpath, config.task, config.data_dim, config.latent_dim, config.n_components, config.lr,
         config.n_samples, config.n_posteriors, config.checkpoint_fpath, config.posterior_params_fpath)
-    trainer = make_trainer(config.dpath, seed, config.n_epochs)
+    trainer = make_trainer(config.dpath, seed, config.n_steps)
     if config.is_test:
         trainer.test(model, data_test)
     else:
@@ -43,8 +43,8 @@ if __name__ == "__main__":
     parser.add_argument("--n_samples", type=int, default=512)
     parser.add_argument("--n_posteriors", type=int, default=512)
     parser.add_argument("--lr", type=float, default=1e-3)
-    parser.add_argument("--batch_size", type=int, default=64)
-    parser.add_argument("--n_epochs", type=int, default=200)
+    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--n_steps", type=int, default=2000)
     parser.add_argument("--n_workers", type=int, default=20)
     parser.add_argument("--is_test", action="store_true")
     main(parser.parse_args())
--- multimodal_dataset_bias_toy/utils/nn_utils.py
+++ multimodal_dataset_bias_toy/utils/nn_utils.py
@@ -4,10 +4,10 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import CSVLogger
 
 
-def make_trainer(dpath, seed, n_epochs):
+def make_trainer(dpath, seed, n_steps):
     return pl.Trainer(
         logger=CSVLogger(dpath, name="", version=seed),
         callbacks=[
             ModelCheckpoint(monitor="val_loss", filename="best")],
-        max_epochs=n_epochs,
+        max_steps=n_steps,
         accelerator="gpu" if torch.cuda.is_available() else "cpu")
