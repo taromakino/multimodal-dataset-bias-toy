b952755ea975fb27490f3787247fbfe3117117da
Taro Makino <taro@nyu.edu>
2023-03-02 19:18:48 -0500

    Select using y, not y_noise

--- multimodal_dataset_bias_toy/data.py
+++ multimodal_dataset_bias_toy/data.py
@@ -42,15 +42,15 @@ def make_standard_data(rng, input_dim, n_examples, u_sd, x_sd, y_sd):
     x1 = u[:, None] ** 2 * np.ones_like(x1_noise) + x1_noise
     x = np.c_[x0, x1]
     y = row_mean(x0 + x1) + y_noise
-    return u.astype("float32"), x.astype("float32"), y.astype("float32"), y_noise.astype("float32")
+    return u.astype("float32"), x.astype("float32"), y.astype("float32")
 
 
 def make_selection_biased_data(rng, input_dim, n_examples, u_sd, x_sd, y_sd, s_shift):
     u_all, x_all, y_all = [], [], []
     count = 0
     while count < n_examples:
-        u, x, y, y_noise = make_standard_data(rng, input_dim, n_examples, u_sd, x_sd, y_sd)
-        collider = u * y_noise
+        u, x, y = make_standard_data(rng, input_dim, n_examples, u_sd, x_sd, y_sd)
+        collider = u * y
         collider = (collider - collider.mean()) / collider.std()
         prob = sigmoid(collider, s_shift)
         s = rng.binomial(1, prob)
@@ -70,10 +70,10 @@ def make_data(seed, input_dim, n_examples, u_sd, x_sd, y_sd, s_shift, is_normali
     n_trainval = n_train + n_val
     rng = np.random.RandomState(seed)
     if s_shift is None:
-        u_trainval, x_trainval, y_trainval, _ = make_standard_data(rng, input_dim, n_trainval, u_sd, x_sd, y_sd)
+        u_trainval, x_trainval, y_trainval = make_standard_data(rng, input_dim, n_trainval, u_sd, x_sd, y_sd)
     else:
         u_trainval, x_trainval, y_trainval = make_selection_biased_data(rng, input_dim, n_trainval, u_sd, x_sd, y_sd, s_shift)
-    u_test, x_test, y_test, _ = make_standard_data(np.random.RandomState(2 ** 32 - 1), input_dim, n_test, u_sd, x_sd, y_sd)
+    u_test, x_test, y_test = make_standard_data(np.random.RandomState(2 ** 32 - 1), input_dim, n_test, u_sd, x_sd, y_sd)
 
     u_train, x_train, y_train = u_trainval[:n_train], x_trainval[:n_train], y_trainval[:n_train]
     u_val, x_val, y_val = u_trainval[n_train:], x_trainval[n_train:], y_trainval[n_train:]
